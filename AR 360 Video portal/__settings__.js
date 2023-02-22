b8a8c6dbba3082fa9051f8b361ea92f33933e762
ciperpugazhenthi <pugazhdhana@gmail.com>
2023-02-22 12:11:08 +0530

    Testing Self hosting from Play canvas o/p
    Self hosting testing of blippar and playcanvas o/p

---
+++ AR 360 Video portal/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1660231.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 121191438, 121288430, 121287936, 121256372, 121256373, 121253807, 121406181, 121572320 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/121303533/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/121303534/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/121303532/1/ammo.js', 'preload' : true},
+];
